163f30d59aecfaf22ea5192e8158d1157df2fbec
Valentin Gita <valentin.gita@strongbytes.ro>
2018-12-22 14:35:25 +0200

    fix names and addresses read from csv
    use csvtojson library
    remove fast-csv dependency

--- csvReader.js
+++ csvReader.js
@@ -1,28 +1,55 @@
-const fastCsv = require('fast-csv');
+const csv = require('csvtojson');
 
 let _names = [];
 let _addresses = [];
 
-let getNames = async function () {
-    if (!_names.length) {
-        await fastCsv
-            .fromPath('names-copy.csv', { headers: true, delimiter: ',' })
-            .on('data', function (data) {
-                _names.push(data.name);
+// let getNames = function () {
+//     if (!_names.length) {
+//          fastCsv
+//             .fromPath('names-copy.csv', { headers: true, delimiter: ',' })
+//             .on('data', function (data) {
+//                 _names.push(data.name);
+//             });
+//     }
+
+//     return _names;
+// };
+
+let getNames = function() {
+    if(!_names.length) {
+        _names = csv()
+            .fromFile('names-copy.csv')
+            .then((jsonObj) => {
+                return jsonObj.map(data => {
+                    return data.name;
+                });
             });
     }
 
     return _names;
-};
+}
+
+// let getAddresses = async  function () {
+//     if (!_addresses.length) {
+//         await fastCsv
+//             .fromPath('addresses-copy.csv', { headers: true, delimiter: ',' })
+//             .on('data', function (data) {
+//                 _addresses.push(
+//                     'City: ' + data.city + ', Street: ' + data.street + ', Zip: ' + data.zip
+//                 );
+//             });
+//     }
+//     return _addresses;
+// };
 
 let getAddresses = async function () {
     if (!_addresses.length) {
-        await fastCsv
-            .fromPath("addresses-copy.csv", { headers: true, delimiter: "," })
-            .on("data", function (data) {
-                _addresses.push(
-                    "City: " + data.city + ", Street: " + data.street + ", Zip: " + data.zip
-                );
+        _addresses = csv()
+            .fromFile('addresses-copy.csv')
+            .then((jsonObj) => {
+                return jsonObj.map(data => {
+                    return 'City: ' + data.city + ', Street: ' + data.street + ', Zip: ' + data.zip;
+                });
             });
     }
     return _addresses;
